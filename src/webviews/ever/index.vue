a807d698dc64d979be85560313b2dc295d44c3f2
Junlei Chen <568538066@qq.com>
2019-03-18 14:32:21 +0800

    toast

--- src/webviews/ever/index.vue
+++ src/webviews/ever/index.vue
@@ -1,12 +1,19 @@
 <template src="./index.html"></template>
 <script>
 	/*CREATE BY JERRY*/
-	import api from 'src/common/util/api.js'
+	import api from 'src/common/util/api.js';
+	import toast from 'src/components/toast/index.js';
 	export default {
 		name: 'ever',
 		methods: {
 			testAPI: () => {
 				api.get('https://www.baicu.com');
+			},
+			testToast: async ()=> {
+				await toast({
+					message: 'abc'
+				})
+				console.log('tosat 关闭')
 			}
 		},
 	}
