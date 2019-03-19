5a5be099d488719440bf2f0bb72f415a834a9d17
Junlei Chen <568538066@qq.com>
2019-03-19 11:11:17 +0800

    model

--- src/webviews/ever/index.vue
+++ src/webviews/ever/index.vue
@@ -3,8 +3,14 @@
 	/*CREATE BY JERRY*/
 	import api from 'src/common/util/api.js';
 	import toast from 'src/components/toast/index.js';
+	import Navigator from 'src/components/navigator/index.vue';
+	import Model from 'src/components/Model/index.vue';
 	export default {
 		name: 'ever',
+		components: {
+			navigator: Navigator,
+			model: Model,
+		},
 		methods: {
 			testAPI: () => {
 				api.get('https://www.baicu.com');
@@ -14,6 +20,9 @@
 					message: 'abc'
 				})
 				console.log('tosat 关闭')
+			},
+			testModel: () => {
+				
 			}
 		},
 	}
